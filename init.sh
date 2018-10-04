2a903ff5aeadc9c52aa5f310c92ae37eeb88cc5d
Charles Gallay <charles.gallay@epfl.ch>
2018-10-04 09:46:17 +0800

    safer dataset download

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 
 #Download the training dataset
 mkdir data
-wget -O data/nyt.tar https://cloud.tsinghua.edu.cn/f/11391e48b72749d8b60a/?dl=1
+wget --tries=0 --read-timeout=20 --continue -O data/nyt.tar https://cloud.tsinghua.edu.cn/f/11391e48b72749d8b60a/?dl=1
+#wget -O data/nyt.tar https://cloud.tsinghua.edu.cn/seafhttp/files/722dbf95-cf28-44cb-b5c6-214726321d62/nyt.tar
 cd data
 tar -xvf nyt.tar
 cd ..
