8edd3256c4d7ba0d2e92372527146c894a2c9918
Charles Gallay <charles.gallay@epfl.ch>
2018-10-04 11:33:01 +0800

    Change URL for google drive

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 #Download the training dataset
 mkdir data
-wget --tries=0 --read-timeout=20 --continue -O data/nyt.tar https://cloud.tsinghua.edu.cn/f/11391e48b72749d8b60a/?dl=1
+wget --tries=0 --read-timeout=20 --continue -O data/nyt.tar https://drive.google.com/uc?export=download&confirm=2DhJ&id=1eSGYObt-SRLccvYCsWaHx1ldurp9eDN_
 #wget -O data/nyt.tar https://cloud.tsinghua.edu.cn/seafhttp/files/722dbf95-cf28-44cb-b5c6-214726321d62/nyt.tar
 cd data
 tar -xvf nyt.tar
